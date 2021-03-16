f0091a4ffad452df52e282723d44364cb2c585c5
yerimkoko <gochi97@naver.com>
2021-03-16 15:06:18 +0900

    ping test

--- config/express.js
+++ config/express.js
@@ -20,6 +20,10 @@ module.exports = function () {
     res.status(200).send('pong');
   });
 
+  app.get('/ping2', (req, res) => {
+    res.status(200).send('pong');
+  });
+
   /* App (Android, iOS) */
   // TODO: 도메인을 추가할 경우 이곳에 Route를 추가하세요.
   require('../src/app/User/userRoute')(app);
